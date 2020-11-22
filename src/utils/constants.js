c49496619da83975e2c2a78fddcffc526da7b3c9
JoseCR-27 <62898109+JoseCR-27@users.noreply.github.com>
2020-11-21 18:37:06 -0600

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,22 @@
+module.exports = {
+	SCREEN: {
+		MAIN: 'Main',
+		PROFILE: 'Profile',
+		HOME: 'Home',
+		DETAILS: 'Details',
+		CASTANDCREW: 'Cast And Crew',
+	},
+	API_KEY: '65cf86856c875b4b1fd81fe55d7d81c1',
+	COLORS: {
+		WARNING: '#FFBC03',
+		LIGHT_GRAY: '#D7D7D7',
+		LIGHT_GRAY2: '#F5F5F5',
+		GRAY: '#ACAEAD',
+		GRAY2: '#ACAEADAA',
+		LIGHT: '#FFFFFF',
+		PRIMARY: '#5FB1A2',
+		PRIMARY2: '#5FB1A25A',
+		TEXT_COLOR: '#313234',
+		TEXT_COLOR2: '#AFB0B0',
+	},
+};
